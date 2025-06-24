a7ca48c8bbdcf2695f6cdc869f774561af48f618
Khanhbroo <khanhmathias532211@gmail.com>
2025-06-24 20:21:49 +0700

    feat: author authentication flow

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import SearchForm from "@/components/search-form";
 import StartupCard, { StartupTypeCard } from "@/components/startup-card";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 export default async function Home({
   searchParams,
@@ -11,6 +12,10 @@ export default async function Home({
   const query = (await searchParams).query;
   const params = { search: query || null };
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+
+  const session = await auth();
+
+  console.log(session);
 
   return (
     <>
